60b9926531e3b9579b8e2857fa1dc460dc697707
AdamFull <logotipick@gmail.com>
2022-07-03 13:14:34 -0700

    HDR returned

--- source/core/graphics/renderpass/render_stages/PostProcessStage.cpp
+++ source/core/graphics/renderpass/render_stages/PostProcessStage.cpp
@@ -19,19 +19,19 @@ void CPostProcessStage::create()
 
     std::string bloomTexture{"composition_tex"};
 
-    /*auto framebuffer_6 = make_scope<CFramebufferNew>();
+    auto framebuffer_6 = utl::make_scope<CFramebufferNew>();
     framebuffer_6->setRenderArea(vk::Offset2D{0, 0}, screenExtent);
-    framebuffer_6->addOutputReference(0U, 0U);
     framebuffer_6->addImage("output_color", vk::Format::eR32G32B32A32Sfloat, vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eSampled);
+    framebuffer_6->addOutputReference(0U, "output_color");
     framebuffer_6->addDescription(0U);
     framebuffer_6->addSubpassDependency(VK_SUBPASS_EXTERNAL, 0, vk::PipelineStageFlagBits::eFragmentShader, vk::PipelineStageFlagBits::eColorAttachmentOutput,
     vk::AccessFlagBits::eShaderRead, vk::AccessFlagBits::eColorAttachmentWrite);
     framebuffer_6->addSubpassDependency(0, VK_SUBPASS_EXTERNAL, vk::PipelineStageFlagBits::eColorAttachmentOutput, vk::PipelineStageFlagBits::eFragmentShader,
     vk::AccessFlagBits::eColorAttachmentWrite, vk::AccessFlagBits::eShaderRead);
-    framebuffer_6->addRenderer(make_scope<CFinalCompositionPass>(bloomTexture));
-    vFramebuffer.emplace_back(std::move(framebuffer_6));*/
+    framebuffer_6->addRenderer(utl::make_scope<CFinalCompositionPass>(bloomTexture));
+    vFramebuffer.emplace_back(std::move(framebuffer_6));
 
-    auto framebuffer_7 = make_scope<CFramebufferNew>();
+    /*auto framebuffer_7 = utl::make_scope<CFramebufferNew>();
     framebuffer_7->setRenderArea(vk::Offset2D{0, 0}, screenExtent);
     framebuffer_7->addImage("output_color", vk::Format::eR32G32B32A32Sfloat, vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eSampled);
     framebuffer_7->addOutputReference(0U, "output_color");
@@ -40,8 +40,8 @@ void CPostProcessStage::create()
     vk::AccessFlagBits::eShaderRead, vk::AccessFlagBits::eColorAttachmentWrite);
     framebuffer_7->addSubpassDependency(0, VK_SUBPASS_EXTERNAL, vk::PipelineStageFlagBits::eColorAttachmentOutput, vk::PipelineStageFlagBits::eFragmentShader,
     vk::AccessFlagBits::eColorAttachmentWrite, vk::AccessFlagBits::eShaderRead);
-    framebuffer_7->addRenderer(make_scope<CThroughPass>(bloomTexture));
-    vFramebuffer.emplace_back(std::move(framebuffer_7));
+    framebuffer_7->addRenderer(utl::make_scope<CThroughPass>(bloomTexture));
+    vFramebuffer.emplace_back(std::move(framebuffer_7));*/
 
     CRenderStage::create();
 }

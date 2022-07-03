444aa80b9e6e67a0c6eb7d696cbaf6176c301227
AdamFull <logotipick@gmail.com>
2022-07-03 13:14:48 -0700

    Variables changed

--- source/core/GlobalVariables.cpp
+++ source/core/GlobalVariables.cpp
@@ -4,8 +4,8 @@ namespace engine
 {
     float GlobalVariables::bloomThreshold{1.0};
 
-    float GlobalVariables::postprocessGamma{1.2};
-    float GlobalVariables::postprocessExposure{4.7f};
+    float GlobalVariables::postprocessGamma{1.8f};
+    float GlobalVariables::postprocessExposure{1.0f};
     float GlobalVariables::blurScale{1.0f};
     float GlobalVariables::blurStrength{1.7f};
 
@@ -15,5 +15,5 @@ namespace engine
     int GlobalVariables::shadingMode{0};
     int GlobalVariables::debugTarget{0};
     int GlobalVariables::debugCascade{0};
-    float GlobalVariables::cascadeSplitLambda{0.75f};
+    float GlobalVariables::cascadeSplitLambda{0.95f};
 }
